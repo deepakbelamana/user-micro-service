cbc1b8eb310aec0c034c48ad385b41e781f6ad6f
deepak belamana <saideepak627@gmail.com>
2023-12-06 11:58:55 +0530

    removed port and host to call microservices by thier application name

---
+++ src/main/java/com/dpkProjects/user/service/models/Hotel.java
@@ -0,0 +1,18 @@
+package com.dpkProjects.user.service.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Hotel {
+
+    private String id;
+    private String name;
+    private String location;
+    private String about;
+}
--- src/main/java/com/dpkProjects/user/service/services/impl/userServiceImpl.java
+++ src/main/java/com/dpkProjects/user/service/services/impl/userServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dpkProjects.user.service.services.impl;
 
 import com.dpkProjects.user.service.exceptions.ResourceNotFoundException;
+import com.dpkProjects.user.service.models.Hotel;
 import com.dpkProjects.user.service.models.Rating;
 import com.dpkProjects.user.service.models.User;
 import com.dpkProjects.user.service.repositories.UserRepo;
@@ -8,11 +9,15 @@ import com.dpkProjects.user.service.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class userServiceImpl implements UserService {
 
@@ -39,9 +44,19 @@ public class userServiceImpl implements UserService {
         User user=userRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("user not exist " + id));
         //get user ratings by calling Rating service using restTemplate
         //http://localhost:8083/ratings/users/userid
-        ArrayList<Rating> ratingsOfUser =  restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
+        Rating[] ratingsOfUser =  restTemplate.getForObject("http://RATING-SERVICE/ratings/users/"+user.getUserId(), Rating[].class);
         logger.info("{} ", ratingsOfUser);
-        user.setRatings(ratingsOfUser);
+        List<Rating>ratings=Arrays.stream(ratingsOfUser).toList();
+        getHotelsByRatings(ratings);
+        user.setRatings(ratings);
         return user;
+    }
+
+    private void getHotelsByRatings(List<Rating> ratings) {
+       ratings.stream().map(rating -> {
+         ResponseEntity<Hotel> hotel = restTemplate.getForEntity("http://HOTEL-SERVICE/hotels/"+rating.getHotelId(), Hotel.class);
+         rating.setHotel(hotel.getBody());
+         return rating;
+        }).collect(Collectors.toList());
     }
 }
